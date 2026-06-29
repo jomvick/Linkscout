db9473fb2c3cecfcf04263cd0bde6d3612ce37e0
jomvick <doriankoanda@gmail.com>
2026-06-29 21:50:28 +0000

    replace SVG favicon with favicon.png

--- linkscout/app/[locale]/layout.tsx
+++ linkscout/app/[locale]/layout.tsx
@@ -25,8 +25,8 @@ export async function generateMetadata({ params }: { params: Promise<{ locale: s
     description: t("dashboardDesc"),
     metadataBase: new URL("https://linkscout-rust.vercel.app"),
     icons: {
-      icon: "/icon.svg",
-      apple: "/apple-icon.svg",
+      icon: "/favicon.png",
+      apple: "/favicon.png",
     },
     robots: {
       index: true,
